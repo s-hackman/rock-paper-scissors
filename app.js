73db32892fd2ec633102714a0e78be1ebee195bb
shishohackman <shisho.hackman@gmail.com>
2022-08-05 19:13:46 +0100

    Add Function playRound

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.random() * 3;
     if (randomNumber <= 1) {
@@ -11,7 +16,17 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        
+    if (playerSelection === "ROCK" && computerSelection === "PAPER" || playerSelection === "PAPER" && computerSelection === "SCISSORS" || playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        computerScore += 1
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else {
+        playerScore += 1
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
 }
+
+
+console.log(playRound(playerSelection, computerSelection));
+
