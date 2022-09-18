b72d8ae03b1705bf560a0c841f812fafb0458068
shishohackman <shisho.hackman@gmail.com>
2022-09-18 19:16:36 +0100

    refactor code and add color to indicate win or loss

--- app.js
+++ app.js
@@ -6,64 +6,81 @@ let roundWinner = "";
 const btnRock = document.getElementById("btn-rock");
 const btnPaper = document.getElementById("btn-paper");
 const btnScissors = document.getElementById("btn-scissors");
-let result = document.querySelector('#result-el')
-let result2 = document.getElementById('result-el-2')
+let result = document.querySelector("#result-el");
+let finalResult = document.getElementById("result-el-2");
 
-let playerScorePara = document.getElementById("player-score")
-let computerScorePara = document.getElementById("computer-score")
+let playerScorePara = document.getElementById("player-score");
+let computerScorePara = document.getElementById("computer-score");
 
-btnRock.addEventListener('click', function() {
-    playerSelection = "ROCK"
-    result.textContent = playRound(playerSelection, getComputerChoice())
-    updateScore()
-})
-btnPaper.addEventListener('click', function() {
-    playerSelection = "PAPER"
-    result.textContent = playRound(playerSelection, getComputerChoice())
-    updateScore()
-})
-btnScissors.addEventListener('click', function() {
-    playerSelection = "SCISSORS"
-    result.textContent = playRound(playerSelection, getComputerChoice())
-    updateScore()
-})
-console.log(computerScore)
+btnRock.addEventListener("click", function () {
+  playerSelection = "ROCK";
+  result.textContent = playRound(playerSelection, getComputerChoice());
+  updateScore();
+});
+btnPaper.addEventListener("click", function () {
+  playerSelection = "PAPER";
+  result.textContent = playRound(playerSelection, getComputerChoice());
+  updateScore();
+});
+btnScissors.addEventListener("click", function () {
+  playerSelection = "SCISSORS";
+  result.textContent = playRound(playerSelection, getComputerChoice());
+  updateScore();
+});
+console.log(computerScore);
 
 function getComputerChoice() {
-    let randomNumber = Math.random() * 3;
-    if (randomNumber <= 1) {
-        return "ROCK"
-    } else if (randomNumber <= 2) {
-        return "PAPER"
-    } else {
-        return "SCISSORS"
-    }
+  let randomNumber = Math.random() * 3;
+  if (randomNumber <= 1) {
+    return "ROCK";
+  } else if (randomNumber <= 2) {
+    return "PAPER";
+  } else {
+    return "SCISSORS";
+  }
 }
 
-
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "ROCK" && computerSelection === "PAPER" || playerSelection === "PAPER" && computerSelection === "SCISSORS" || playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        computerScore += 1
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else {
-        playerScore += 1
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
+  if (
+    (playerSelection === "ROCK" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "ROCK")
+  ) {
+    computerScore++;
+    result.classList.add("lose");
+    result.classList.remove("win");
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    result.classList.remove("lose");
+    result.classList.remove("win");
+    return "It's a tie!";
+  } else {
+    playerScore++;
+    result.classList.add("win");
+    result.classList.remove("lose");
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
 }
 
 function updateScore() {
-    playerScorePara.textContent = `Player: ${playerScore}`
-    computerScorePara.textContent = `Computer: ${computerScore}`
-    if(playerScore === 5){
-        result2.textContent ='You Win the game'
-        playerScore = 0;
-        computerScore = 0;
-    } else if (computerScore === 5){
-        result2.textContent= 'You Lose the game'
-        computerScore = 0;
-        playerScore = 0;
-    }
+  playerScorePara.textContent = `Player: ${playerScore}`;
+  computerScorePara.textContent = `Computer: ${computerScore}`;
+  if (playerScore === 5) {
+    finalResult.textContent = "You Win the game";
+    finalResult.classList.add("win");
+  } else if (computerScore === 5) {
+    finalResult.textContent = "You Lose the game";
+    finalResult.classList.add("lose");
+  }
 }
 
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  result.textContent = "";
+  finalResult.textContent = "First to score 5 points wins the game";
+  finalResult.classList.remove("win");
+  finalResult.classList.remove("lose");
+  updateScore();
+});
